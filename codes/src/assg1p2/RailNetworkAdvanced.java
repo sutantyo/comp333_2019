e77c1e2103d938c57ea431e14861a0f4337a5713
Daniel Sutantyo <daniel.sutantyo@gmail.com>
2019-09-02 02:36:49 +1000

    added files for Stages 3 and 4

---
+++ codes/src/assg1p2/RailNetworkAdvanced.java
@@ -0,0 +1,265 @@
+package assg1p2;
+
+import java.io.*;
+import java.util.*;
+
+import assg1p1.Station;
+
+
+public class RailNetworkAdvanced {
+
+	private TreeMap<String,Station> stationList;
+	
+	public RailNetworkAdvanced(String trainData, String connectionData, String lineData) {
+		stationList = new TreeMap<>();
+		
+		try {	
+			readLinesData(lineData);
+			readStationData(trainData);
+			readConnectionData(connectionData);
+		}
+		catch (IOException e) {
+			System.out.println("Exception encountered: " + e);
+		}
+	}
+	
+	/**
+	 * Reads the CSV file containing information about the lines
+	 * 
+	 * @param infile
+	 * @throws IOException
+	 */
+	public void readLinesData(String infile) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(infile));
+		/*
+		 * INSERT YOUR CODE HERE
+		 */
+
+	}
+	/**
+	 * Reads the CSV file containing information about the stations and 
+	 * populate the TreeMap<String,Station> stationList. Each row of 
+	 * the CSV file contains the name, latitude and longitude coordinates
+	 * of the station.
+	 * 
+	 * You need to make a Station object for each row and add it to the 
+	 * TreeMap<String,Station> stationList where the key value is the 
+	 * name of the station (as a String).
+	 * 
+	 * @param infile	   the path to the file
+	 * @throws IOException if the file is not found
+	 */
+	public void readStationData(String infile) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(infile));
+		/*
+		 * INSERT YOUR CODE HERE
+		 */
+		in.close();
+	}
+	/**
+	 * Reads the CSV file containing information about connectivity between 
+	 * adjacent stations, and update the stations in stationList so that each
+	 * Station object has a list of adjacent stations.
+	 * 
+	 * Each row contains two Strings separated by comma. To obtain the distance
+	 * between the two stations, you need to use the latitude and longitude 
+	 * coordinates together with the computeDistance() methods defined below
+	 * 
+	 * @param infile	   the path to the file
+	 * @throws IOException if the file is not found
+	 */	
+	public void readConnectionData(String infile) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(infile));
+		/*
+		 * INSERT YOUR CODE HERE
+		 */
+		in.close();
+	}
+	
+	/**
+	 * Given the latitude and longitude coordinates of two locations x and y, 
+	 * return the distance between x and y in metres using Haversine formula,
+	 * rounded down to the nearest integer.
+	 * 
+	 * Note that two more methods are provided below for your convenience 
+	 * and you should not directly call this method
+	 * 
+	 * source://www.geeksforgeeks.org/haversine-formula-to-find-distance-between-two-points-on-a-sphere/
+	 * 
+	 * @param lat1 latitude coordinate of x
+	 * @param lon1 longitude coordinate of x
+	 * @param lat2 latitude coordinate of y
+	 * @param lon2 longitude coordinate of y
+	 * @return distance betwee
+	 */
+	public static int computeDistance(double lat1, double lon1, double lat2, double lon2) {
+        // distance between latitudes and longitudes 
+        double dLat = Math.toRadians(lat2 - lat1); 
+        double dLon = Math.toRadians(lon2 - lon1); 
+  
+        // convert to radians 
+        lat1 = Math.toRadians(lat1); 
+        lat2 = Math.toRadians(lat2); 
+  
+        // apply formulae 
+        double a = Math.pow(Math.sin(dLat / 2), 2) +  
+                   Math.pow(Math.sin(dLon / 2), 2) *  
+                   Math.cos(lat1) *  
+                   Math.cos(lat2); 
+        double rad = 6371.0; 
+        Double c = 2 * Math.asin(Math.sqrt(a)); 
+        Double distance = rad * c * 1000;
+        return distance.intValue(); 
+	}	
+	
+	/**
+	 * Compute the distance between two stations in metres, where the stations
+	 * are given as String objects
+	 * 
+	 * @param a		the first station
+	 * @param b		the second station
+	 * @return		the distance between the two stations in metres
+	 */
+	public int computeDistance(String a, String b) {
+		Station u = stationList.get(a);
+		Station v = stationList.get(b);
+		return computeDistance(u.getLatitude(),u.getLongitude(),
+							   v.getLatitude(),v.getLongitude());
+	}
+	
+	/**
+	 * Compute the distance between two stations in metres, where the stations
+	 * are given as Station objects
+	 * 
+	 * @param a		the first station
+	 * @param b		the second station
+	 * @return		the distance between the two stations in metres
+	 */
+	public int computeDistance(Station a, Station b) {
+		return computeDistance(a.getLatitude(),a.getLongitude(),
+							   b.getLatitude(),b.getLongitude());
+	}
+
+	/**
+	 * The method finds the shortest route (in terms of number of stops)
+	 * between the origin station and the destination station.
+	 * The route is returned as an ArrayList<String> containing the names of 
+	 * the stations along the route, including the origin and the destination 
+	 * stations.
+	 * 
+	 * If the route cannot be completed (there is no path between origin and
+	 * destination), then return an empty ArrayList<String>
+	 * 
+	 * If the origin or the destination stations are not in the list of stations,
+	 * return an empty ArrayList<String>. 
+	 * 
+	 * If the origin and the destination stations are the same, return an 
+	 * ArrayList<String> containing the station.
+	 * 
+	 * @param origin		the starting station
+	 * @param destination	the destination station
+	 * @return
+	 */
+	public ArrayList<String> routeMinStop(String origin, String destination){
+		if (!stationList.containsKey(origin) || !stationList.containsKey(destination)) {
+			return new ArrayList<String>();
+		}
+		if (origin.equals(destination)) {
+			ArrayList<String> ans = new ArrayList<String>();
+			ans.add(origin);
+			return ans;
+		}
+		/*
+		 * INSERT YOUR CODE HERE
+		 */
+		return null;
+	}
+
+	
+	/**
+	 * Given a route between two stations, compute the total distance 
+	 * of this route.
+	 * 
+	 * @param path	the list of stations in the route (as String objects)
+	 * @return		the length of the route between the first station
+	 * 				and last station in the list	
+	 */
+	public int findTotalDistance(ArrayList<String> path) {
+		int distance = 0;
+		/*
+		 * INSERT YOUR CODE HERE
+		 */
+		return distance;
+	}
+	
+
+	
+	/**
+	 * Return the ratio between the length of the shortest route between two
+	 * stations (in terms of distance) and the actual distance between the 
+	 * two stations (computed using computeDistance())
+	 * 
+	 * In other words, 
+	 * let d1 = distance of shortest route between the two stations as computed
+	 *          by the method routeMinStop() (from Stage 1).
+	 * let d2 = distance between two stations as computed by the method
+	 *          computeDistance() 
+	 *          
+	 * The method returns d1/d2 (as a double)
+	 * 
+	 * @param origin		the starting station
+	 * @param destination 	the ending station
+	 * @return	s			the ratio d1/d2 as explained above
+	 */
+	public double computeRatio(String origin, String destination) {
+		return 0.0;
+	}
+	
+	
+	/**
+	 * Return the ratio as computed by computeRatio() method for all 
+	 * pairs of station in the rail network.
+	 * 
+	 * The ratios should be stored in a HashMap<String,HashMap<String,Double>>,
+	 * that is, the ratio between station a and b can be obtained by calling
+	 * 
+	 *    computeAllRatio().get(a).get(b)
+	 * 
+	 * @return a hashmap containing the ratios
+	 */
+	public HashMap<String,HashMap<String,Double>> computeAllRatio() {
+		return null;
+	}
+	
+	/**
+	 * The method finds the shortest route (in terms of number of stops)
+	 * between the origin station and the destination station, taking 
+	 * into account the available routes in the rail network.
+	 * 
+	 * The route is returned as an ArrayList<String> containing the lines taken,
+	 * any transfer between lines, and the names of the stations on each line,
+	 * including the origin and the destination stations.
+	 * 
+	 * Please see the assignment specification for more information.
+	 * 
+	 * If the route cannot be completed (there is no path between origin and
+	 * destination), then return an empty ArrayList<String>
+	 * 
+	 * If the origin or the destination stations are not in the list of stations,
+	 * return an empty ArrayList<String>. 
+	 * 
+	 * If the origin and the destination stations are the same, return an 
+	 * ArrayList<String> containing the station.
+	 * 
+	 * @param origin		the starting station
+	 * @param destination	the end station
+	 * @return				the route taken
+	 */
+	public ArrayList<String>routeMinStopWithRoutes(String origin, String destination){
+		
+		return null;
+	}
+	
+	
+	
+}
