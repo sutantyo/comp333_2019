7eda6a3d2187233747331a6b34401f09afb9e269
Daniel Sutantyo <daniel.sutantyo@gmail.com>
2019-09-29 17:01:09 +1000

    added tests for Stage3

---
+++ codes/src/assg1p2/Stage3Test.java
@@ -0,0 +1,75 @@
+package assg1p2;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Stage3Test {
+
+	
+	Double epsilon = 0.000001;
+
+	RailNetworkAdvanced r;
+	@Before public void initialize() {
+		String stationData = "src/data/station_data.csv";
+		String connectionData = "src/data/adjacent_stations.csv";
+		String linesData = "src/data/lines_data.csv";
+		r = new RailNetworkAdvanced(stationData,connectionData,linesData);
+	}	
+	
+	@Test
+	public void computeAllRatioTest1() {
+
+		String origin = "Parramatta";
+		String destination = "Strathfield";
+		
+		HashMap<String,HashMap<String,Double>> ans = r.computeAllRatio();
+		Double expected = 1.097584920479088;
+		Double actual = ans.get(origin).get(destination);
+
+		assertEquals(178,ans.size());
+		assertEquals(expected,actual,epsilon);
+	}
+
+	@Test
+	public void computeAllRatioTest2() {
+		String origin = "Bankstown";
+		String destination = "Hornsby";
+		
+		HashMap<String,HashMap<String,Double>> ans = r.computeAllRatio();
+		Double expected = 1.3356612123918958;
+		Double actual = ans.get(origin).get(destination);
+
+		assertEquals(178,ans.size());
+		assertEquals(expected,actual,epsilon);
+	}
+	
+	@Test
+	public void computeAllRatioTest3() {
+		String origin = "Richmond";
+		String destination = "Hurstville";
+		
+		HashMap<String,HashMap<String,Double>> ans = r.computeAllRatio();
+		Double expected = 1.3687600644122384;
+		Double actual = ans.get(origin).get(destination);
+
+		assertEquals(178,ans.size());
+		assertEquals(expected,actual,epsilon);
+	}
+	
+	@Test
+	public void computeAllRatioTest4() {
+		String origin = "Hurstville";
+		String destination = "Richmond";
+		
+		HashMap<String,HashMap<String,Double>> ans = r.computeAllRatio();
+		Double expected = 1.3687600644122384;
+		Double actual = ans.get(origin).get(destination);
+
+		assertEquals(178,ans.size());
+		assertEquals(expected,actual,epsilon);
+	}
+}
